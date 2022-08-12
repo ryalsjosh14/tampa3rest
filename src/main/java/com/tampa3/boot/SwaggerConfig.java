95924ec40f6870f5a401ace2abdad7fff437d879
Eduardo Fajardo <emakenso@outlook.es>
2022-08-12 19:47:40 +0000

    Changed the apiInfo and groupName in Swagger

--- src/main/java/com/tampa3/boot/SwaggerConfig.java
+++ src/main/java/com/tampa3/boot/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     @Bean
     public Docket newsApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("compactdiscs")
+                .groupName("Holding")
                 .apiInfo(apiInfo())
                 .select()
                 .paths(PathSelectors.any())
@@ -26,10 +26,10 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Album REST API with Swagger")
-                .description("This API allows you to interact with albums. It is a CRUD API")
+                .title("Stocks REST API with Swagger")
+                .description("This API allows you to interact with your online Portfolio. It is a CRUD API")
                 //.termsOfServiceUrl("http://www.conygre.com")
-                .contact(new Contact("Nick Todd", "http://www.conygre.com", "nick.todd@conygre.com"))
+                //.contact(new Contact("Nick Todd", "http://www.conygre.com", "nick.todd@conygre.com"))
                 //.license("Apache License Version 2.0")
                 //.licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
                 //.version("2.0")
